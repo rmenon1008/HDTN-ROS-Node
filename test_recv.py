7846b5574a0b19391bbdf53089442d48f813abb0
rmenon1008 <rmenon1008@gmail.com>
2023-03-07 14:42:11 -0800

    Adds acks for sent files

--- test_recv.py
+++ test_recv.py
@@ -2,21 +2,23 @@ from src.hdtn import HDTN_Receiver
 import time
 import logging
 
+logging.basicConfig(level=logging.INFO)
+
 settings = {
     "eid": "ipn:2.1",
     "unpack_after_recv": True,
     "unpack_recv_dir": "received_items",
 }
 
-logging.basicConfig(level=logging.INFO)
+def on_item_received(item):
+    print("Received item:", item)
 
 # Setting up an HDTN instance
-hdtn = HDTN_Receiver(settings, hdtn_root="../HDTN")
+hdtn = HDTN_Receiver(settings, hdtn_root="../HDTN", recv_callback=on_item_received)
 
 # Start hdtn
 hdtn.start()
 
-# Monitor the status
+# Busy wait
 while True:
     time.sleep(2)
-    print("Received items:", hdtn.received_items)
--- test_send.py
+++ test_send.py
@@ -2,28 +2,30 @@ from src.hdtn import HDTN_Sender
 import time
 import logging
 
+logging.basicConfig(level=logging.INFO)
+
 settings = {
     "eid": "ipn:1.1",
     "dest_host": "localhost",
     "dest_eid": "ipn:2.1",
 }
 
-logging.basicConfig(level=logging.INFO)
+def on_item_sent(item):
+    print("Sent item:", item)
 
 # Setting up an HDTN instance
-hdtn = HDTN_Sender(settings, hdtn_root="../HDTN")
+hdtn = HDTN_Sender(settings, hdtn_root="../HDTN", send_callback=on_item_sent)
 
 # Start hdtn
 hdtn.start()
 
-time.sleep(2)
+# Send a file
+hdtn.send_item("test_files/test.txt")
+time.sleep(5)
 
-hdtn.send_item("srv")
+# Send a directory
+hdtn.send_item("test_files/test_dir")
 
-time.sleep(20)
-
-hdtn.send_item("kill.sh")
-
+# Busy wait
 while True:
     time.sleep(2)
-    print("Sent items:", hdtn.sent_items)
